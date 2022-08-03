864f04e4d6a042379f99f9d878502295bcefdf8d
Aaroncul <aaronocullen@hotmail.co.uk>
2022-08-03 16:58:10 +0100

    Add basic Customer Entity and persistence.xml

---
+++ src/main/java/com/allstate/claimsserver/model/Customer.java
@@ -0,0 +1,59 @@
+package com.allstate.claimsserver.model;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
